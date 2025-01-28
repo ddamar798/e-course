57bd51ef33e86b82e44113619b8ebcc8c5ea472a
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-01-28 09:29:09 +0700

    feat: add scema table benefits

--- database/migrations/2025_01_28_020320_create_course_benefits_table.php
+++ database/migrations/2025_01_28_020320_create_course_benefits_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('course_benefits', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('courese_id')->constrained()->cascadeOnDelete();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
