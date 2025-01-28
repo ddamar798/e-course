0c3a822ab5f29399cb30d8f878f403277cd53094
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-01-28 09:37:29 +0700

    fix: in section content

--- database/migrations/2025_01_28_020324_create_course_students_table.php
+++ database/migrations/2025_01_28_020324_create_course_students_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('course_students', function (Blueprint $table) {
             $table->id();
+            $table->boolean('is_active');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('courese_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
